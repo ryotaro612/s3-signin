7a8f1495007c32a8483b62685e9208d5caf0188c
Ryotaro <ryotaro612.ooo@gmail.com>
2025-01-22 02:58:48 +0900

    feat: put a text file by terraform

---
+++ deployments/terraform/bucket.tf
@@ -0,0 +1,33 @@
+// aws s3 presign s3://amzn-s3-demo-bucket/mydoc.txt --expires-in 604800 --region af-south-1 --endpoint-url https://s3.af-south-1.amazonaws.com
+
+variable "aws_region" {
+  type = string
+}
+variable "aws_profile" {
+  type = string
+}
+variable "bucket_name" {
+  type = string
+}
+
+
+provider "aws" {
+  region = var.aws_region
+  profile = var.aws_profile
+}
+
+resource "aws_s3_bucket" "sample_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_object" "sample_object" {
+  bucket = var.bucket_name
+  key    = "index.txt"
+  source = "index.txt"
+}
+
